8dc273b42ca48af562d4d50e3b8f6d1ba412f2a3
AyeshaShamail <ayeshashamail1931@gmail.com>
2022-01-10 16:54:20 +0530

    added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,10 @@
+public class MulTwoNumbers {
+
+   public static void main(String[] args) {
+        
+      int num1 = 15, num2 = 5, product;
+      product = num1 * num2;
+
+      System.out.println("Product of these numbers: "+product);
+   }
+}
